bdc64a46f2c8e3b4a21bc1e4e94cd7c26c285fad
diva-D <davidgentile@gmail.com>
2019-01-29 13:27:02 -0500

    Complete checkpoint 5

---
+++ basic-calc.js
@@ -0,0 +1,7 @@
+exports.add = function(num1, num2) {
+    console.log(num1 + num2);
+}
+
+exports.subtract = function(num1, num2) {
+    console.log(num1 - num2);
+}
--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const greetings = require("./greeting.js");
+const calculator = require("./basic-calc")
 
 // "Hello"
 greetings.greetingEnglish();
 
 // "Konichiwa"
 greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+
+// 4
+calculator.add(2, 2);
+
+// 5
+calculator.subtract(10, 5);
